01c3acef0c33cc89cc8045f07d2d0f9d2f669ed7
KriiLunAus <kriilunaus16@gmail.com>
2023-12-30 21:35:04 +0100

    task-3(js)

--- js/task-3.js
+++ js/task-3.js
@@ -1,2 +1,16 @@
 `use strict`
 
+const textInput = document.getElementById("name-input");
+
+const textOutput = document.getElementById("name-output");
+
+
+textInput.addEventListener("input", (event) => {
+    const trimedOutput = event.currentTarget.value.trim();
+
+    if (trimedOutput !== "") {
+        textOutput.textContent = trimedOutput;
+    } else {
+        textOutput.textContent = "Anonymous!"
+    }
+});
